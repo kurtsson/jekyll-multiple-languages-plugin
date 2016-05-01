6fa5ff386601b7c1f6f3d5a969e51bc6bd785d5b
Anthony Gaudino <anthonygaudino.45@gmail.com>
2016-05-01 20:31:31 -0300

    Issue #38: Due to posts having to be placed under subfolders to separate the multiple languages content, while using the permalink ":categories" option on the _config.yml, the building process added the subfolders paths to the categories, generating wrong permalinks. This fix monkey patches the Jekyll permalink population method to remove the unwanted strings.

--- lib/jekyll/multiple/languages/plugin.rb
+++ lib/jekyll/multiple/languages/plugin.rb
@@ -162,6 +162,32 @@ module Jekyll
 
 
 
+  ##############################################################################
+  # class Post
+  ##############################################################################
+  class Post
+      
+      alias :populate_categories_org :populate_categories
+      
+      #======================================
+      # populate_categories
+      #
+      # Monkey patched this method to remove unwanted strings
+      # ("_i18n" and language code) that are prepended to posts categories
+      # because of how the multilingual posts are arranged in subfolders.
+      #======================================
+    def populate_categories
+      categories_from_data = Utils.pluralized_array_from_hash(data, 'category', 'categories')
+      self.categories = (
+        Array(categories) + categories_from_data
+      ).map {|c| c.to_s.downcase}.flatten.uniq
+      
+      self.categories.delete("_i18n")
+      self.categories.delete(site.config['lang'])
+      
+      return self.categories
+    end
+  end
 
 
 
