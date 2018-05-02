04596fc54a0f1732226370c96dd79f14fc6d6df2
Alessandro Descovi <descovi@gmail.com>
2018-05-02 19:36:41 +0200

    remover update

--- lib/tools/remover.rb
+++ lib/tools/remover.rb
@@ -2,15 +2,21 @@ module Jekyll
   module MultipleLanguagePluginTools
 
     class Remover
-      def self.go static_file_relative_path, exclude_paths
-        exclude_paths.any? do |exclude_path|
-          Pathname.new(static_file_relative_path).descend do |static_file_path|
-            path_equivalent_tool = PathEquivalent.new(exclude_path, static_file_path)
-            break(true) if path_equivalent_tool.check
-          end
+      def self.go static_file_relative_path, excluded_paths
+        excluded_paths.any? do |excluded_path|
+          same_path?(static_file_relative_path, excluded_path)
+        end
+      end
+
+      private
+
+      def self.same_path? static_file_relative_path, excluded_path
+        Pathname.new(static_file_relative_path).descend do |static_file_path|
+          path_equivalent_tool = PathEquivalent.new(excluded_path, static_file_path)
+          break(true) if path_equivalent_tool.check
         end
       end
     end
-    
+
   end
 end
