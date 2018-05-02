97490e560c81aa7186854cc421fea82a9691102f
Alessandro Descovi <descovi@gmail.com>
2018-05-02 15:30:45 +0200

    test for remover

--- test/base_test.rb
+++ test/base_test.rb
@@ -1,26 +1,23 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'jekyll'
-require './lib/jekyll-multiple-languages-plugin.rb'
-class TestMeme < Minitest::Test
-  def setup
-    #exclude_path = 'file.rb'
-    # Pathname.new('/path/to/some/file.rb').descend do |static_file_path|
-    #   @remover = Jekyll::FileRemover.new(exclude_path, static_file_path)
-    # end
-  end
+require './lib/tools.rb'
 
-  def test_that_kitty_can_eat
-    static_path = Pathname.new('file.rb')
-    equivalent_tool = Jekyll::PathEquivalentTool.new('file.rb', static_path)
-    assert equivalent_tool.check
+module Jekyll
+  module MultipleLanguagesTools
+    class TestTools < Minitest::Test
+
+      def test_delete_from_array
+        static_files  = ['file.jpeg', 'file.txt', '/relative_path.pdf'].map{|value| Pathname.new(value)}
+        exclude_paths = ['file.jpeg']
+        static_files.delete_if do |static_file|
+          Remover.go static_file, exclude_paths
+        end
+        assert_equal static_files, ['file.txt', '/relative_path.pdf'].map{|value| Pathname.new(value)}
+      end
+
+      def test_path_are_the_same
+        equivalent_tool = PathEquivalent.new('file.rb', Pathname.new('file.rb'))
+        assert equivalent_tool.check
+      end
   end
-  #
-  # def test_that_it_will_not_blend
-  #   refute_match /^no/i, @meme.will_it_blend?
-  # end
-  #
-  # def test_that_will_be_skipped
-  #   skip "test this later"
-  # end
 end
