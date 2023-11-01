622297653b8f0ebf5c25e0db4adac8a2de5f714d
Alexander Nickol <a.nickol@trust-nickol.de>
2023-11-01 09:22:16 +0100

    Update jekyll-multiple-languages-plugin.rb
    Ruby 3.0 compatibility

--- lib/jekyll-multiple-languages-plugin.rb
+++ lib/jekyll-multiple-languages-plugin.rb
@@ -48,7 +48,7 @@ module Jekyll
             if (exclude_path == f_path)
               new_path = parts[0] + "/" + f_path
               puts "Moving '" + file_path + "' to '" + new_path + "'"
-              if (Dir.exists?(new_path))
+              if (Dir.exist?(new_path))
                 FileUtils.rm_r new_path
               end
               File.rename file_path, new_path
